41a26eaba46e829e865a30a05bad7c69827cdf8f
Asmaa Sayed <Asmaa.Sayed@ibtikar.com>
2020-10-18 15:48:24 +0200

    First time

---
+++ src/test/java/TestPackage/FinaalProject/TestngClass.java
@@ -0,0 +1,16 @@
+package TestPackage.FinaalProject;
+
+import org.testng.annotations.Test;
+
+public class TestngClass {
+	
+	
+	
+@Test
+void printMsg()
+{
+	System.out.println(" thanks for gud");
+}
+	
+	
+}
